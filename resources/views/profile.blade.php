47f54cbb0561f548b94c1856526a6dd161bd86c2
Petar <parandjic@gmail.com>
2019-09-25 01:21:45 +0200

    This page is  for loged in user. Manage they posts

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.master')
-
+@section('style')
+    <link rel="stylesheet" href="{{ URL::asset('css/style.css') }}">
+@endsection
 @section('container')
+    <div class="container-fluid">
 
+    </div>
 @endsection

d344ab3f2afbc6efa07c84811654ce1cf5f2fcea
Petar <parandjic@gmail.com>
2019-09-25 21:28:49 +0200

    Made Model and Migration for vehicle

---
+++ database/migrations/2019_09_24_233707_create_vehicles_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVehiclesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vehicles', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('category_id');
+            $table->bigInteger('profile_id');
+            $table->string('name');
+            $table->double('price', 8 , 2);
+            $table->year('manufacture_year');
+            $table->bigInteger('mileage');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vehicles');
+    }
+}

bb772d20310cd17ff669d5b9d4dafd679b4097ba
Petar <parandjic@gmail.com>
2019-09-28 18:41:15 +0200

    Added constructor for auth, made create, store fun

--- app/Http/Controllers/VehicleController.php
+++ app/Http/Controllers/VehicleController.php
@@ -4,7 +4,31 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+
 class VehicleController extends Controller
 {
+    function __construct(){
+        $this->middleware('auth');
+    }
     //
+    public function create(){
+        return view('vehicle.create');
+    }
+
+    //
+    public function store(){
+        //dd(request()->all());
+        $data = request()->validate([
+            'name' => 'required',
+            'category' => 'required',
+            'manufacture_year' => 'required',
+            'mileage' => 'required',
+            'price' => 'required',
+            'vehicle_image' => ['required', 'image']
+        ]);
+
+        auth()->user()->vehicles()->create($data);
+
+        dd(request()->all());
+    }
 }

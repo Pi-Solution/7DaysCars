bfafa7a036f30d07ff0d3074b65afc5d3d2dcb71
Petar <parandjic@gmail.com>
2019-09-28 18:44:48 +0200

    not needed right now

--- resources/views/profile.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.master')
-@section('style')
-    <link rel="stylesheet" href="{{ URL::asset('css/style.css') }}">
-@endsection
-@section('container')
-    <div class="container-fluid">
-
-    </div>
-@endsection

ea0867ec70935172dc829788ebdc5826aaa5e4e0
Petar <parandjic@gmail.com>
2019-09-28 18:42:50 +0200

    added paths for logout on button

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,10 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
+//Added this to be able to use button for logout
+use Auth;
+use Illuminate\Http\Request;
+
 class LoginController extends Controller
 {
     /*
@@ -35,5 +39,13 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+    /**
+     * Logout
+     *
+     */
+    public function logout(Request $request) {
+        Auth::logout();
+        return redirect('/');
     }
 }

3674674a41ba76715437a40eb6e77fc026ce3c8c
Petar <parandjic@gmail.com>
2019-09-28 18:44:15 +0200

    imported nav_bar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,11 @@
-@extends('layouts.app')
+@extends('layouts.master')
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+@section('nav_bar')
+    @include('layouts.nav_bar')
+@endsection
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+@section('container')
+    <div class="container-fluid p-0 m-0">
 
-                    You are logged in!
-                </div>
-            </div>
-        </div>
     </div>
-</div>
 @endsection

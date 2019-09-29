6a90a01151a568bd23372e3a815f1b4e898a7519
Petar <parandjic@gmail.com>
2019-09-29 21:45:53 +0200

    Made VehicleCategory Model, Factory, Seeder, Migr

---
+++ database/factories/VehicleCategoryFactory.php
@@ -0,0 +1,13 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\VehicleCategory;
+use Faker\Generator as Faker;
+
+$factory->define(VehicleCategory::class, function (Faker $faker) {
+
+    return [
+
+    ];
+});
---
+++ database/seeds/VehicleCategoryTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class VehicleCategoryTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $data = ['MICRO', 'SEDAN', 'CUV', 'SUV', 'MINIVAN', 'HATCHBACK', 'ROADSTER', 'PICKUP', 'VAN', 'COUP', 'TRUCK', 'BIG TRUCK'];
+        //
+        foreach ($data as $name) {
+            factory(App\VehicleCategory::class, 1)->create([
+                'name' => $name
+            ]);
+        }
+    }
+}

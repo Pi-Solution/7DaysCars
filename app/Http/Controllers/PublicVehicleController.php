ec6acb7016e216e5143a952ec3429db793a06764
Petar <parandjic@gmail.com>
2019-10-15 00:05:47 +0200

    md fun for show all Veh and show all veh by cat

--- app/Http/Controllers/PublicVehicleController.php
+++ app/Http/Controllers/PublicVehicleController.php
@@ -4,10 +4,52 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Vehicle;
+
+use App\VehicleCategory;
+
 class PublicVehicleController extends Controller
 {
-    //
-    public function show(){
-        return view('welcome');
+    /**
+     * Show all vehicles
+     *
+     */
+    public function index(){
+
+        $vehicles = Vehicle::all();
+
+        //Get vehicle maker
+        foreach ($vehicles as $vehicle) {
+           $vehicle->maker = $vehicle->VehicleMaker->name;
+        }
+
+        $categories = VehicleCategory::all();
+
+        return view('welcome', [
+            'vehicles' => $vehicles,
+            'categories' =>$categories
+        ]);
+    }
+    /**
+     * Show Vehicles by category
+     *
+     */
+    public function show($id){
+
+        # Check if user want to see all vehicles or by category
+        if ($id == 0) {
+            //Get vehicle maker
+            $vehicles = Vehicle::all();
+            foreach ($vehicles as $vehicle) {
+                $vehicle->maker = $vehicle->VehicleMaker->name;
+            }
+           return $vehicles;
+        }else{
+            $vehicles = VehicleCategory::findOrFail($id)->vehicles;
+            foreach ($vehicles as $vehicle) {
+                $vehicle->maker = $vehicle->VehicleMaker->name;
+            }
+            return $vehicles;
+        }
     }
 }
